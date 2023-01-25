0d504f4689062f6e9a6672d387be2eae18e91acb
Aryan Chandra <aryanchandra7360@gmail.com>
2023-01-25 08:57:42 +0530

    Made some changes . Purpose = ??

--- Practical_02.py
+++ Practical_02.py
@@ -15,6 +15,8 @@
 
 
 # Solution
+import matplotlib.pyplot as plt
+import copy
 
 class iris:
     def __init__(self, sepalLength, sepalWidth, petalLength, petalWidth, species):
@@ -25,12 +27,36 @@ class iris:
         self.species = species
 
     def value(self):
-        temp = self.sepalLength + ',' + self.sepalWidth + ',' + self.petalLength + ',' + self.petalWidth + ',' + self.species
+        temp = self.sepalLength + ',' + self.sepalWidth + ',' + \
+            self.petalLength + ',' + self.petalWidth + ',' + self.species
+        return temp
+
+    def checkConstraiont(self):
+        temp = [0, 0, 0, 0, 0]
+        speciesValue = ['"setosa"\n', '"versicolor"\n', '"virginica"\n']
+
+        if (self.sepalLength == 'NA' or self.sepalWidth == 'NA' or self.petalLength == 'NA' or self.petalWidth == 'NA'):
+            return temp
+        if (self.species in speciesValue):
+            temp[0] = 0
+        else:
+            temp[0] = 1
+
+        if (float(self.sepalLength) < 0 or float(self.sepalWidth) < 0 or float(self.petalLength) < 0 or float(self.petalWidth) < 0):
+            temp[1] = 1
+        if (float(self.petalLength) < 2*float(self.petalWidth)):
+            temp[2] = 1
+        if (float(self.sepalLength) > 30.0):
+            temp[3] = 1
+        if (float(self.sepalLength) < float(self.petalLength) or float(self.sepalWidth) < float(self.petalWidth)):
+            temp[4] = 1
+
         return temp
 
 
 irisList = []
 fileLine = []
+originalIrisList = []
 
 file = open("dirty_iris.csv", "r")
 for x in file:
@@ -42,6 +68,7 @@ for x in range(0, len(fileLine)):
     temp = fileLine[x].split(',')
     irisList.append(iris(temp[0], temp[1], temp[2], temp[3], temp[4]))
 
+originalIrisList = copy.deepcopy(irisList)
 irisLength = len(irisList)-1
 
 incompleteData = 0
@@ -56,20 +83,20 @@ print("No. of Incomplete Data : ", incompleteData)
 try:
     print("Percentage of Incomplete Data : ", (incompleteData/irisLength)*100)
 except:
-    print("File is Empty")   
+    print("File is Empty")
 
 # (ii) part starts Here.
 
 for l in range(0, len(irisList)):
     try:
-        if (float(irisList[l].sepalLength) <= 0.000):
+        if (float(irisList[l].sepalLength) <= 0.000 or float(irisList[l].sepalLength) > 10.00):
             irisList[l].sepalLength = "NA"
-        if (float(irisList[l].sepalWidth) <= 0.0000):
+        if (float(irisList[l].sepalWidth) <= 0.0000 or float(irisList[l].sepalWidth) > 10.00 ):
             irisList[l].sepalWidth = "NA"
-        if (float(irisList[l].petalLength) <= 0.0000):
-            irisList[l].petalLength = "NA"           
-        if (float(irisList[l].petalWidth) <= 0.0000):
-            irisList[l].petalWidth = "NA"           
+        if (float(irisList[l].petalLength) <= 0.0000 or float(irisList[l].petalLength) > 10.00 ):
+            irisList[l].petalLength = "NA"
+        if (float(irisList[l].petalWidth) <= 0.0000 or float(irisList[l].petalWidth) > 10.00):
+            irisList[l].petalWidth = "NA"
     except:
         continue
 
@@ -78,4 +105,40 @@ fileWrite = open("dirty_iris.csv", "w")
 for l in range(0, len(irisList)):
     fileWrite.write(irisList[l].value())
 
+fileWrite.close()
 
+ruleVoilation = [0, 0, 0, 0, 0]
+
+for l in range(1, len(irisList)):
+    result = originalIrisList[l].checkConstraiont()
+    for k in range(0, 5):
+        ruleVoilation[k] = ruleVoilation[k] + result[k]
+
+print("Species Rule Voilation : " , ruleVoilation[0])
+print("Positive Value Rule Voilation : " , ruleVoilation[1])
+print("Petal Length Rule Voilation : " , ruleVoilation[2])
+print("Sepal Length Rule Voilation : " , ruleVoilation[3])
+print("Sepal and Petal Length Rule Voilation : " , ruleVoilation[4])
+
+Options = ["Species Rule Voilation" ,"Positive Value Rule Voilation","Petal Length Rule Voilation","Sepal Length Rule Voilation","Sepal and Petal Length Rule Voilation"]
+
+
+plt.bar(Options, ruleVoilation, color ='maroon',
+        width = 0.4)
+plt.ylabel('No. of Rule Voilation')
+plt.title("No. of Record : " + str(len(irisList)-1))
+plt.show()
+
+sepalLengthList = []
+
+for l in range(1 , len(irisList)):
+    try:
+     sepalLengthList.append(float(irisList[l].sepalLength))
+    except:
+        continue
+
+graph = plt.boxplot(sepalLengthList)
+plt.title("Sepal Length")
+plt.show()
+
+print(graph)
